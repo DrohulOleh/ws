062f878433d17cf7ee28c3b9e592c4564d4ec06e
Drohul Oleh <oleh@drohul.com>
2024-03-17 15:04:15 +0200

    qq

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HttpClientModule, RouterOutlet],
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private auth: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    const localStorage = this.document.defaultView?.localStorage;
+
+    if (localStorage) {
+      const potentialToken = localStorage.getItem('auth-token');
+
+      if(potentialToken) {
+        this.auth.setToken(potentialToken);
+      }
+    }
+  }
 }
