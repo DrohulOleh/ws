158d83fea7693807a01ee5abe4b302cf998efcb5
Drohul Oleh <oleh@drohul.com>
2024-03-19 18:52:57 +0200

    components

--- client/src/app/containers/auth-layout/auth-layout.component.ts
+++ client/src/app/containers/auth-layout/auth-layout.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [],
-  templateUrl: './auth-layout.component.html'
+  imports: [RouterOutlet],
+  templateUrl: './auth-layout.component.html',
 })
-export class AuthLayoutComponent {
-
-}
+export class AuthLayoutComponent {}
