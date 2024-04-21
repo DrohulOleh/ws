214b4002d24ba0151b45c5c19fbccbebb0272c29
Drohul Oleh <oleh@drohul.com>
2024-04-21 14:21:27 +0300

    profile Preparation

--- client/src/app/pages/profile-page/profile-page.component.ts
+++ client/src/app/pages/profile-page/profile-page.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
   imports: [],
-  templateUrl: './profile-page.component.html'
+  templateUrl: './profile-page.component.html',
 })
-export class ProfilePageComponent {
+export class ProfilePageComponent implements OnInit, OnDestroy {
+  emailConfirmed: boolean = false;
+  formEditProfile: FormGroup | any;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.formEditProfile = new FormGroup({
+      profileName: new FormControl(null, [Validators.required]),
+      profileEmail: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+      ]),
+      profilePassword: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      profileNewPassword: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      profileNewPasswordConfirm: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      profilDeliveryAddress: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  ngOnDestroy(): void {}
 }
