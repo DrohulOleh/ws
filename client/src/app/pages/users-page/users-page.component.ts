093fb1dcbea86b201f0a235b89a7abc4f80872df
Drohul Oleh <oleh@drohul.com>
2024-04-28 18:47:49 +0300

    user role change function added

--- client/src/app/pages/users-page/users-page.component.ts
+++ client/src/app/pages/users-page/users-page.component.ts
@@ -1,21 +1,49 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { IOrder, IUser } from '../../shared/classes/types';
+import { EUserRoles, IOrder, IUser } from '../../shared/classes/types';
 import { AuthService } from '../../shared/services/auth.service';
-import { SpinnerModule, TableModule } from '@coreui/angular';
+import {
+  BadgeModule,
+  ButtonModule,
+  FormModule,
+  ModalModule,
+  SpinnerModule,
+  TableModule,
+} from '@coreui/angular';
 import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { OrderService } from '../../shared/services/order.service';
+import { IconModule } from '@coreui/icons-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-page',
   standalone: true,
-  imports: [TableModule, CommonModule, SpinnerModule],
+  imports: [
+    TableModule,
+    CommonModule,
+    SpinnerModule,
+    ButtonModule,
+    ModalModule,
+    BadgeModule,
+    IconModule,
+    FormModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './users-page.component.html',
 })
 export class UsersPageComponent implements OnInit, OnDestroy {
   orders: IOrder[] = [];
   users: IUser[] = [];
   usersWithOrders: any[] = [];
+  selectedUser!: any | undefined;
+  selectedRole!: any | undefined;
+  userRoles: EUserRoles[] = Object.values(EUserRoles);
+
+  modalDetailedViewVISIBLE = false;
+  modalChangeRoleVISIBLE = false;
+  checkedAgreement = false;
+  radioChecked = false;
 
   userSubscription!: Subscription;
   orderSubscription!: Subscription;
@@ -51,8 +79,6 @@ export class UsersPageComponent implements OnInit, OnDestroy {
               // return an object with data about the user and his orders
               return { ...user, orders: userOrders, totalOrders, totalAmount };
             });
-
-            console.log(this.usersWithOrders);
           },
         });
       },
@@ -70,5 +96,63 @@ export class UsersPageComponent implements OnInit, OnDestroy {
     }, 0);
   }
 
-  changeRole(user: IUser) {}
+  calculateOrderItems(order: IOrder) {
+    return order.list?.reduce((acc, item) => {
+      return (acc += item.quantity || 0);
+    }, 0);
+  }
+
+  detailedView(userId: string) {
+    this.selectedUser = this.usersWithOrders.find(
+      (user) => user._id === userId
+    );
+    if (this.selectedUser) {
+      //console.log(this.selectedUser);
+      this.toggleModalDetailedView();
+    }
+  }
+
+  changeRole() {
+    this.checkedAgreement = false;
+    this.toggleModalChangeRole();
+  }
+
+  onCheckAgreement() {
+    this.checkedAgreement = !this.checkedAgreement;
+  }
+
+  onCheckRole(value: any) {
+    console.log(value);
+    this.selectedRole = value;
+    return value;
+  }
+
+  onSubmitChangeRole(userId: string) {
+    console.log(userId, this.selectedRole);
+
+    this.userSubscription = this.authService.getUserById(userId).subscribe({
+      next: (user) => {
+        if (user) {
+          console.log(this.selectedRole);
+          user.role = this.selectedRole ? this.selectedRole : user.role;
+          this.authService.updateUser(user).subscribe();
+          this.ngOnInit();
+          this.toggleModalChangeRole();
+          this.toggleModalDetailedView();
+        }
+      },
+      error: (err) => {
+        console.log(err);
+        this.toggleModalChangeRole();
+      },
+    });
+  }
+
+  toggleModalDetailedView() {
+    this.modalDetailedViewVISIBLE = !this.modalDetailedViewVISIBLE;
+  }
+
+  toggleModalChangeRole() {
+    this.modalChangeRoleVISIBLE = !this.modalChangeRoleVISIBLE;
+  }
 }
